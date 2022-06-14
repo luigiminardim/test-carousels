c59d4a97f61359499113ffc4a407a2d1206918e7
luigiminardim <luigiminardim@gmail.com>
2022-06-14 17:25:26 -0300

    feat: :sparkles: test slick.

---
+++ pages/slick.tsx
@@ -0,0 +1,82 @@
+import {
+  Button,
+  IconButton,
+  Container,
+  Image,
+  Center,
+  Box,
+} from "@chakra-ui/react";
+import SlickCarousel from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { banners } from "./src/banners";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+
+export default function Slick() {
+  const mobileBanners = banners.filter((banner) => banner.size === "mobile");
+  const desktopBanners = banners.filter((banner) => banner.size === "desktop");
+
+  return (
+    <>
+      <Container maxW={"container.xl"} display={{ base: "block", lg: "none" }}>
+        <SlickCarousel
+          dots={true}
+          infinite={true}
+          speed={500}
+          slidesToShow={1}
+          slidesToScroll={1}
+        >
+          {mobileBanners.map((banner) => (
+            <Image
+              key={banner.src}
+              htmlWidth={1920}
+              htmlHeight={1200}
+              width="full"
+              height="auto"
+              alt="carousel"
+              loading="lazy"
+              src={banner.src}
+            />
+          ))}
+        </SlickCarousel>
+      </Container>
+      <Box>
+        <Container
+          maxW={"container.xl"}
+          display={{ base: "none", lg: "block" }}
+        >
+          <SlickCarousel
+            dots={true}
+            infinite={true}
+            speed={500}
+            slidesToShow={1}
+            slidesToScroll={1}
+            autoplay={true}
+          >
+            {desktopBanners.map((banner) => (
+              <Image
+                key={banner.src}
+                htmlWidth={1920}
+                htmlHeight={450}
+                width="full"
+                height="auto"
+                alt="carousel"
+                loading="lazy"
+                src={banner.src}
+              />
+            ))}
+          </SlickCarousel>
+          <IconButton
+            aria-label=""
+            position="absolute"
+            right="0px"
+            top="50%"
+            transform="translateY(-50%)"
+          >
+            <ChevronRightIcon />
+          </IconButton>
+        </Container>
+      </Box>
+    </>
+  );
+}
---
+++ pages/src/banners.ts
@@ -0,0 +1,22 @@
+export const banners = [
+  {
+    src: "https://ibassets.com.br/ib.store.banner/bnr-4620cb59379f40b2a3ec6036520a5a63.jpeg",
+    size: "desktop",
+  },
+  {
+    src: "https://ibassets.com.br/ib.store.banner/bnr-68221d4850224d2faf5c9336358e431e.jpeg",
+    size: "desktop",
+  },
+  {
+    src: "https://ibassets.com.br/ib.store.banner/bnr-b53d0d22a7294fb497f797c5c76c2b0a.jpeg",
+    size: "mobile",
+  },
+  {
+    src: "https://ibassets.com.br/ib.store.banner/bnr-652905efedb64bd08f2a264f3e764ecf.jpeg",
+    size: "desktop",
+  },
+  {
+    src: "https://ibassets.com.br/ib.store.banner/bnr-15d22a71c0554e93bb62277ea409e4aa.jpeg",
+    size: "mobile",
+  },
+];
